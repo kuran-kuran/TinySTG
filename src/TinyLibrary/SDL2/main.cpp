968dfd03fb0f446a4304e0d47c04d815a8d8a7ce
user <kuran_kuran@ka2.so-net.ne.jp>
2022-12-23 21:37:03 +0900

    自機をコントロールできなかったので修正した、無視リスト追加

--- src/TinyLibrary/SDL2/main.cpp
+++ src/TinyLibrary/SDL2/main.cpp
@@ -9,12 +9,30 @@
 int main(int argc, char* argv[])
 {
 	MainLoop_Setup();
+
+	Uint64 beforeTime= SDL_GetPerformanceCounter();
+	Uint64 intervalTime = SDL_GetPerformanceFrequency() / Screen::FPS;
 	bool quit = false;
 	while(quit == false)
 	{
-        MainLoop_Loop();
-		MainLoop_NoWaitLoop();
-//		SDL_Delay(1000 / CONFIG_FPS);
+		Uint64 nowTime = SDL_GetPerformanceCounter();
+		Uint64 elapsed = nowTime - beforeTime;
+		if(elapsed > intervalTime)
+		{
+			SDL_Event event;
+			while(SDL_PollEvent(&event))
+			{
+				switch(event.type)
+				{
+				case SDL_QUIT:
+					quit = true;
+					break;
+				}
+			}
+			MainLoop_Loop();
+			MainLoop_NoWaitLoop();
+			beforeTime = nowTime;
+		}
 	}
 	return 0;
 }
